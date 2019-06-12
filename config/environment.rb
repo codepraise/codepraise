ae5ae7c8b782c449281e8e6501bd839cb50b1442
vicxu <xumingyo@gmail.com>
2019-06-12 21:46:07 +0800

    set session expiration value as one month

--- config/environment.rb
+++ config/environment.rb
@@ -6,13 +6,17 @@ require 'econfig'
 module CodePraise
   # Environment-specific configuration
   class App < Roda
+    HOUR = 60 * 60
+    DAY = 24 * HOUR
+    MONTH = 30 * DAY
     plugin :environments
 
     extend Econfig::Shortcut
     Econfig.env = environment.to_s
     Econfig.root = '.'
 
-    use Rack::Session::Cookie, secret: config.SESSION_SECRET
+    use Rack::Session::Cookie, secret: config.SESSION_SECRET,
+                               expire_after: MONTH
 
     configure :development, :test do
       require 'pry'
