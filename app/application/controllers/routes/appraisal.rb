907e20b3d09e9df2d5557036379af594db873f19
tienyulin <t.tienyulin@gmail.com>
2022-11-01 20:27:21 +0800

    feat: add cache control and etag

--- app/application/controllers/routes/appraisal.rb
+++ app/application/controllers/routes/appraisal.rb
@@ -4,6 +4,7 @@ module CodePraise
   # appraisal routing
   class App < Roda
     plugin :multi_route
+    plugin :request_headers
 
     route('appraisal') do |routing|
       routing.on String, String do |owner_name, project_name|
@@ -36,6 +37,20 @@ module CodePraise
         end
 
         routing.get do
+          cache_control = Cache::Control.new(response)
+          cache_control.turn_on if Env.new(Api).production?
+          if cache_control.on?
+            if_none_match = request.env['HTTP_IF_NONE_MATCH']
+            redis = CodePraise::Cache::Client.new(CodePraise::App.config)
+            etag_key = "#{owner_name}_#{project_name}_etag"
+            etag_value = redis.get(etag_key)
+
+            if !if_none_match.nil? && !etag_value.nil? && if_none_match.include?(etag_value)
+              response.status = 304
+              return response.to_json
+            end
+          end
+
           @path = 'appraisal'
 
           path_request = ProjectRequestPath.new(
@@ -78,6 +93,10 @@ module CodePraise
             view 'progress_bar', locals: { path: @path, processing: @processing,
                                            project_path: @project_path, panel_view: @panel_view }
           else
+            if cache_control.on?
+              etag_value = redis.get(etag_key)
+              response['Etag'] = etag_value
+            end
             view 'dashboard', locals: { path: @path, processing: @processing,
                                         project_path: @project_path, panel_view: @panel_view }
           end
